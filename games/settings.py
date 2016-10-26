8609c119b90e3be9c6387129d1e5df3869710d8e
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2016-10-26 23:35:58 +0300

    Fixed statics

--- games/settings.py
+++ games/settings.py
@@ -132,3 +132,8 @@ if DEPLOY:
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
