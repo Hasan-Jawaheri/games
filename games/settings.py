d87e6ad318d0fb8391f8e3d7cf15fdaf605fe52a
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2016-10-29 17:28:02 +0300

    Cleaned staticfiles

--- games/settings.py
+++ games/settings.py
@@ -131,4 +131,4 @@ if DEPLOY:
     # https://docs.djangoproject.com/en/1.9/howto/static-files/
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+    #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
